bc846d38aef189e63d5ce3bd6c640baa390a9b65
joungtao <joungtao@gmail.com>
2017-04-18 17:37:17 +0800

    改一下

--- typy/cpy/typy.c
+++ typy/cpy/typy.c
@@ -9,83 +9,6 @@
 extern "C" {
 #endif
 
-void FormatTypeError(PyObject* arg, const char* err) {
-	PyObject* repr = PyObject_Repr(arg);
-	if (!repr) { return; }
-	PyErr_Format(PyExc_TypeError, "%s%.100s has type %.100s",
-		err, PyString_AsString(repr), Py_TYPE(arg)->tp_name);
-	Py_DECREF(repr);
-}
-
-PyBytes Typy_CheckBytes(PyObject* arg, const char* err) {
-	if (!arg || arg == Py_None) {
-		FormatTypeError(arg, err);
-		return NULL;
-	} else if (PyUnicode_Check(arg)) {
-		return (PyBytes)PyUnicode_AsEncodedObject(arg, "utf-8", NULL);
-	} else if (PyBytes_Check(arg)) {
-		Py_INCREF(arg);
-		return (PyBytes)arg;
-	} else {
-		FormatTypeError(arg, err);
-		return NULL;
-	}
-}
-
-bool isDefaultEncodingUTF8 = false;
-
-static const char module_docstring[] =
-"python-proto2 is a module that can be used to enhance proto2 Python API\n"
-"performance.\n"
-"\n"
-"It provides access to the protocol buffers C++ reflection API that\n"
-"implements the basic protocol buffer functions.";
-
-static PyObject* SetDefaultEncodingUTF8(PyObject* m) {
-	PyUnicode_SetDefaultEncoding("utf-8");
-	isDefaultEncodingUTF8 = true;
-	Py_RETURN_NONE;
-}
-
-static PyMethodDef ModuleMethods[] = {
-	{"setDefaultEncodingUTF8", (PyCFunction)SetDefaultEncodingUTF8, METH_NOARGS,
-		"sys.setdefaultencoding('utf-8') to get better performance for string."},
-	{"Object", (PyCFunction)Typy_RegisterObject, METH_VARARGS,
-		"register typy Object with properties."},
-	{"Variant", (PyCFunction)Typy_RegisterVariant, METH_VARARGS,
-		"register typy Variant with properties."},
-	{"Enum", (PyCFunction)Typy_RegisterEnum, METH_VARARGS,
-		"register typy Enum with properties."},
-	{"List", (PyCFunction)Typy_RegisterList, METH_VARARGS,
-		"register typy List with properties."},
-	{"Dict", (PyCFunction)Typy_RegisterDict, METH_VARARGS,
-		"register typy Dict with properties."},
-	{"Python", (PyCFunction)Typy_RegisterPython, METH_VARARGS,
-		"register typy Python with properties."},
-	{"FixedPoint", (PyCFunction)Typy_RegisterFixedPoint, METH_VARARGS,
-		"register typy FixedPoint with properties."},
-	{ NULL, NULL}
-};
-
-#if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef _module = {
-	PyModuleDef_HEAD_INIT,
-	FULL_MODULE_NAME,
-	module_docstring,
-	-1,
-	ModuleMethods, /* m_methods */
-	NULL,
-	NULL,
-	NULL,
-	NULL
-};
-#define INITFUNC PyInit__typyd
-#define INITFUNC_ERRORVAL NULL
-#else // Python 2
-#define INITFUNC init_typyd
-#define INITFUNC_ERRORVAL
-#endif
-
 static PyIntObject kPythonZeroObject;
 static PyIntObject kint32min_obj;
 static PyIntObject kint32max_obj;
@@ -158,6 +81,87 @@ static PyTypeObject* PyTypeObjects[] = {
 	&TypyVariantType,
 	NULL,
 };
+
+void FormatTypeError(PyObject* arg, const char* err) {
+	PyObject* repr = PyObject_Repr(arg);
+	if (!repr) { return; }
+	PyErr_Format(PyExc_TypeError, "%s%.100s has type %.100s",
+		err, PyString_AsString(repr), Py_TYPE(arg)->tp_name);
+	Py_DECREF(repr);
+}
+
+PyBytes Typy_CheckBytes(PyObject* arg, const char* err) {
+	if (!arg || arg == Py_None) {
+		FormatTypeError(arg, err);
+		return NULL;
+	} else if (PyUnicode_Check(arg)) {
+		return (PyBytes)PyUnicode_AsEncodedObject(arg, "utf-8", NULL);
+	} else if (PyBytes_Check(arg)) {
+		Py_INCREF(arg);
+		return (PyBytes)arg;
+	} else {
+		FormatTypeError(arg, err);
+		return NULL;
+	}
+}
+
+bool isDefaultEncodingUTF8 = false;
+
+static const char module_docstring[] =
+"python-proto2 is a module that can be used to enhance proto2 Python API\n"
+"performance.\n"
+"\n"
+"It provides access to the protocol buffers C++ reflection API that\n"
+"implements the basic protocol buffer functions.";
+
+static PyObject* SetDefaultEncodingUTF8(PyObject* m) {
+	PyUnicode_SetDefaultEncoding("utf-8");
+	isDefaultEncodingUTF8 = true;
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef ModuleMethods[] = {
+	{"setDefaultEncodingUTF8", (PyCFunction)SetDefaultEncodingUTF8, METH_NOARGS,
+		"sys.setdefaultencoding('utf-8') to get better performance for string."},
+	{"Object", (PyCFunction)Typy_RegisterObject, METH_VARARGS,
+		"register typy Object with properties."},
+	{"Variant", (PyCFunction)Typy_RegisterVariant, METH_VARARGS,
+		"register typy Variant with properties."},
+	{"Enum", (PyCFunction)Typy_RegisterEnum, METH_VARARGS,
+		"register typy Enum with properties."},
+	{"List", (PyCFunction)Typy_RegisterList, METH_VARARGS,
+		"register typy List with properties."},
+	{"Dict", (PyCFunction)Typy_RegisterDict, METH_VARARGS,
+		"register typy Dict with properties."},
+	{"Python", (PyCFunction)Typy_RegisterPython, METH_VARARGS,
+		"register typy Python with properties."},
+	{"FixedPoint", (PyCFunction)Typy_RegisterFixedPoint, METH_VARARGS,
+		"register typy FixedPoint with properties."},
+	{ NULL, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef _module = {
+	PyModuleDef_HEAD_INIT,
+	FULL_MODULE_NAME,
+	module_docstring,
+	-1,
+	ModuleMethods, /* m_methods */
+	NULL,
+	NULL,
+	NULL,
+	NULL
+};
+#	ifndef INITFUNC
+#		define INITFUNC PyInit__typyd
+#	endif
+#	define INITFUNC_ERRORVAL NULL
+#else // Python 2
+#	ifndef INITFUNC
+#		define INITFUNC init_typyd
+#	endif
+#	define INITFUNC_ERRORVAL
+#endif
 
 PyMODINIT_FUNC INITFUNC(void) {
 	TypyIntObject_INIT(kPythonZero, 0);
